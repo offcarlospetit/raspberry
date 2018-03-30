ef8279ff9d70abb0690b7432ef1d6e38490c8f55
Carlos Alberto Petit Quintero <ca.alberto.p@gmail.com>
2018-03-30 14:17:00 -0300

    Animacion

--- App.js
+++ App.js
@@ -19,20 +19,68 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const SCREN_HEIGHT = Dimensions.get('window').height;
-const SCREN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 type Props = {};
 export default class App extends Component<Props> {
 
   componentWillMount(){
-      this.animation = new Animated.ValueXY({x:0, y:SCREN_HEIGHT-80});
+      this.animation = new Animated.ValueXY({x:0, y:SCREEN_HEIGHT-90});
+
+      this.panResponder = PanResponder.create({
+          
+          onMoveShouldSetPanResponder:()=>true,
+
+          onPanResponderGrant:(evt,gestureState)=>{
+              this.animation.extractOffset()
+          },
+
+          onPanResponderMove:(evt,gestureState)=>{
+              this.animation.setValue({x:0,y:gestureState.dy})
+          },
+
+          onPanResponderRelease:(evt,gestureState)=>{
+              if (gestureState.dy < 0 ){
+                  Animated.spring(this.animation.y,{
+                      toValue: -SCREEN_HEIGHT + 120,
+                      tension: 1
+                  }).start()
+              }
+              else if(gestureState>0){
+                  Animated.spring(this.animation.y,{
+                      toValue: SCREEN_HEIGHT - 120,
+                      tension: 1
+                  }).start()
+              }
+          }
+      })
   }
 
   render() {
       const animatedHeigth = {
           transform : this.animation.getTranslateTransform()
-      }
+      };
+      animatedImageHeight = this.animation.y.interpolate({
+          inputRange:[0,SCREEN_HEIGHT - 90],
+          outputRange:[200, 32],
+          extrapolate: 'clamp'
+      });
+      animatedSongTitleOpacity = this.animation.y.interpolate({
+          inputRange:[0, SCREEN_HEIGHT - 500, SCREEN_HEIGHT - 90],
+          outputRange:[0, 0, 1],
+          extrapolate: 'clamp'
+      });
+      animatedImageMarginLeft = this.animation.y.interpolate({
+          inputRange:[0, SCREEN_HEIGHT - 90],
+          outputRange:[SCREEN_WIDTH / 2 - 100, 10],
+          extrapolate: 'clamp'
+      });
+      animatedHeaderHeight = this.animation.y.interpolate({
+          inputRange:[0, SCREEN_HEIGHT - 90],
+          outputRange:[SCREEN_HEIGHT / 2 ,90],
+          extrapolate: 'clamp'
+      });
     return (
       <Animated.View
           style={{flex:1, backgroundColor:'white'}}>
@@ -45,18 +93,20 @@ export default class App extends Component<Props> {
                         right:0,
                         zIndex:10,
                         backgroundColor:'white',
-                        height:SCREN_HEIGHT
+                        height:SCREEN_HEIGHT
                     }
                   ]
               } >
-              <Animated.View style={{height:80, borderTopWidth:1, borderTopColor:'#ebe5e5', flexDirection:'row', alignItems:'center'}}>
+              <Animated.View
+                  {... this.panResponder.panHandlers}
+                  style={{height:animatedHeaderHeight, borderTopWidth:1, borderTopColor:'#ebe5e5', flexDirection:'row', alignItems:'center'}}>
                   <View style={{flex:4, flexDirection:'row', alignItems:'center'}}>
-                      <Animated.View style={{height:32, width:32, marginLeft:10}}>
+                      <Animated.View style={{height:animatedImageHeight, width:animatedImageHeight, marginLeft:animatedImageMarginLeft}}>
                           <Image style={{flex:1, width:null, height:null}} source={require('./assets/me.png')}/>
                       </Animated.View>
-                      <Animated.Text style={{opacity:1, fontSize:18, paddingLeft:10}}>Hotel california</Animated.Text>
+                      <Animated.Text style={{opacity:animatedSongTitleOpacity, fontSize:18, paddingLeft:10}}>Hotel california</Animated.Text>
                   </View>
-                  <Animated.View style={{flex:1, flexDirection:'row', opacity:1, justifyContent:'space-around'}}>
+                  <Animated.View style={{flex:1, flexDirection:'row', opacity:animatedSongTitleOpacity, justifyContent:'space-around'}}>
                       <Icon name='pause' size={32} style={{ color: 'black' }}/>
                       <Icon name='play' size={32} style={{ color: 'black' }}/>
                   </Animated.View>
